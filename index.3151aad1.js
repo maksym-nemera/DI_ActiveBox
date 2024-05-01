46842a7408af30ff268259349733e2e6bad46423
Maksym Nemera <nickmitchell@ukr.net>
2024-05-01 19:07:25 +0300

    Updates

---
+++ index.3151aad1.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){$(".sidenav").sidenav()}),$(document).ready(function(){$(".materialboxed").materialbox()});//# sourceMappingURL=index.3151aad1.js.map
+
+//# sourceMappingURL=index.3151aad1.js.map
